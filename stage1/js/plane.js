802599734f839f582ac741a465e0d68bdfd0081e
syd84 <email@ukr.net>
2026-02-05 16:28:08 +0200

    logic done for the first level

---
+++ stage1/js/plane.js
@@ -0,0 +1,66 @@
+"use strict";
+
+class CoordinateSystem {
+  width = document.body.clientWidth;
+  height = document.body.clientHeight;
+  x0 = this.width / 2;
+  y0 = this.height / 2;
+}
+
+let coordinateSystem = new CoordinateSystem();
+
+class Plane {
+
+  plane_box = document.getElementById("plane_box");
+  plane = document.getElementById("plane");
+  width = plane_box.clientWidth;
+  height = plane_box.clientHeight;
+  xc = this.plane_box.style.left - this.width / 2;
+  yc = this.plane_box.style.top - this.height / 2;
+  hp = 100;
+  destroyed = false;
+
+  fly() {
+    let x0 = coordinateSystem.x0 - this.width / 2;
+    let y0 = coordinateSystem.y0 - this.height / 2;
+    let amplitudeX = coordinateSystem.x0 - 2 * this.width;
+    let amplitudeY = coordinateSystem.y0 - 2 * this.height;
+    let amplitude = Math.min(amplitudeX, amplitudeY);
+    let count = 0;
+    let timePlane = setInterval(() => {
+      if (this.crash()) {
+        clearInterval(timePlane);
+        return;
+      };
+      this.hpLine();
+      const dx = amplitude * Math.sin(2 * count * Math.PI / 180);
+      const dy = amplitude * Math.cos(3 * count * Math.PI / 180);
+      const ddx = amplitude * Math.sin(2 * (count + 1) * Math.PI / 180);
+      const ddy = amplitude * Math.cos(3 * (count + 1) * Math.PI / 180);
+
+      this.xc = coordinateSystem.x0 + dx;
+      this.yc = coordinateSystem.y0 + dy;
+
+      plane_box.style.left = x0 + dx + "px";
+      plane_box.style.top = y0 + dy + "px";
+
+      let angle = Math.atan2(ddy - dy, ddx - dx);
+
+      this.plane.style.transform = `rotate(${angle}rad)`;
+      count++;
+    }, 40);
+  }
+
+  crash() {
+    if (this.hp <= 0) {
+      this.destroyed = true;
+      plane_box.remove();
+      return true;
+    }
+  }
+
+  hpLine() {
+    document.getElementById("hp_line").style.width = `${this.hp}%`;
+  }
+}
+
---
+++ stage1/js/rockets.js
@@ -0,0 +1,51 @@
+"use strict";
+
+class Rocket {
+  constructor(x, y, numId) {
+    this.id = `rocket${numId}`;
+    this.xc = x;
+    this.yc = y;
+    this.destroyed = false;
+    let rocket = document.createElement("div");
+    rocket.id = `rocket${numId}`;
+    rocket.classList.add("rocket");
+    rocket.style.left = this.xc + "px";
+    rocket.style.top = this.yc + "px";
+    document.body.append(rocket);
+
+    this.rocketDiv = document.getElementById(this.id);
+    this.width = this.rocketDiv.clientWidth;
+    this.height = this.rocketDiv.clientHeight;
+  }
+
+  persecution(velocity = 1) {
+    this.rocketDiv.style.left = this.xc - this.width / 2 + "px";
+    this.rocketDiv.style.top = this.yc - this.height / 2 + "px";
+
+    let timeRocket = setInterval(() => {
+      if (this.clash() || plane.destroyed) {
+        clearInterval(timeRocket);
+      }
+      let angle = Math.atan2(plane.yc - this.yc, plane.xc - this.xc);
+      this.rocketDiv.style.transform = `rotate(${angle}rad)`;
+      let dx = velocity * Math.cos(angle);
+      let dy = velocity * Math.sin(angle);
+      this.xc += dx;
+      this.yc += dy;
+      this.rocketDiv.style.left = this.xc - this.width / 2 + "px";
+      this.rocketDiv.style.top = this.yc - this.height / 2 + "px";
+    }, 40)
+  }
+
+  clash() {
+    if (distance(this.xc, this.yc, plane.xc, plane.yc) <= ((plane.width + this.width) / 2) && this.rocketDiv.classList.contains("rocket")) {
+      if (!this.destroyed) {
+        plane.hp -= 10;
+      }
+      this.destroyed = true;
+      this.rocketDiv.remove();
+      return true;
+    }
+  }
+}
+
