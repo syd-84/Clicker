6a22d44fc615f319337aec468cd2f4bdc42b4862
syd84 <email@ukr.net>
2026-02-16 00:25:37 +0200

    changed esc menu button

--- menu/js/js.js
+++ menu/js/js.js
@@ -34,14 +34,20 @@ btnsTag[0].addEventListener("click", () => {
   }, 500)
 });
 
+btnsTag[1].addEventListener("click", () => {
+  setTimeout(() => {
+    // window.location.href = './stage2/stage_2.html'
+  }, 500)
+});
+
 btnsTag[2].addEventListener("click", () => {
   setTimeout(() => {
     window.location.href = './stage3/stage_3.html'
   }, 500)
 });
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    this.body.style.display = "none";
-  }
+btnsTag[3].addEventListener("click", function () {
+  setTimeout(() => {
+    document.body.innerHTML = "";
+  }, 500)
 });
